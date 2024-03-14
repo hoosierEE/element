9983cd478d5ff15d60e79e2e1ccb7b5b36e59ff5
Alex Shroyer <ashroyer@gmail.com>
2024-03-14 13:16:58 -0400

    differentiate between -1 and - 1

--- src/misc/Scanner.py
+++ src/misc/Scanner.py
@@ -1,32 +1,27 @@
 class Scanner:
- ''' -a2+b3*51a => [- a2 + b3 * 51 a] '''
+ ''' -a2+b3*51a*-5 => [- a2 + b3 * 51 a * - 5] '''
  def __init__(s,expr:str):
   s.s = [*expr] # individual characters
   s.i = 0 # current index
   s.z = len(expr)
   s.tokens = s.tokenize()
   assert len(expr) == sum(map(len,s.tokens)), 'wat happen'
+ def peek(s): return s.next(0)
+ def next(s,inc=1):
+  if s.i>=s.z: return ''
+  x = s.s[s.i]
+  s.i += inc
+  return x
  def tokenize(s):
   ts = []
   while s.peek():
    t = ''
    if s.peek().isnumeric(): # [0-9]+ not followed by [A-Za-z]
-    while s.peek().isnumeric():
-     t += s.next()
+    while s.peek().isnumeric(): t += s.next()
     ts.append(t)
-    if s.peek().isalpha():
-     raise NameError("Numbers can't have letters in them.")
+    if s.peek().isalpha(): raise NameError("Numbers can't have letters in them.")
    elif s.peek().isalpha(): # [A-Za-z][0-9A-Za-z]*
-    while s.peek().isalnum():
-     t += s.next()
+    while s.peek().isalnum(): t += s.next()
     ts.append(t)
-   else: # 1-char non-alphanumeric symbol
-    ts.append(s.next())
+   else: ts.append(s.next())
   return ts
- def peek(s): return s.next(0)
- def next(s,inc=1):
-  if s.i>=s.z:
-   return ''
-  x = s.s[s.i]
-  s.i += inc
-  return x
--- src/misc/ee.py
+++ src/misc/ee.py
@@ -6,155 +6,163 @@ debug = print
 # def debug(*args,**kwargs): pass
 
 bop = { #{token : (bp, arity, name)}
- '+': (30,2,'add'),
- '-': (30,2,'sub'),
- '*': (30,2,'mul'),
- '%': (30,2,'div'),
- '^': (30,2,'pow'),
- '=': (30,2,'equal'),
- '[': (98,2,'fun'), # funcall (arity=2): (f: function name, [..]: argument list)
- ']': (99,0,'arglist'),
- ')': (99,0,'cparen'),
+ '+': (10,2,'add'),
+ '-': (10,2,'sub'),
+ '*': (10,2,'mul'),
+ '%': (10,2,'div'),
+ '^': (10,2,'pow'),
+ '=': (10,2,'equal'),
  ';': (90,2,'semi'),
+ '[': (98,2,'fun'), # funcall (arity=2): (f: function name, [..]: argument list)
+ ')': (99,0,'cparen'),
+ '': (100,0,'finalizer'),
 }
 
 uop = {
- '-': (30,1,'negate'),
- '*': (30,1,'first'),
- '+': (30,1,'flip'),
- '=': (30,1,'group'),
- '%': (30,1,'sqrt'),
+ '-': (10,1,'negate'),
+ '*': (10,1,'first'),
+ '+': (10,1,'flip'),
+ '=': (10,1,'group'),
+ '%': (10,1,'sqrt'),
  '(': (99,0,'list'), # start group
  ']': (99,0,'empty_arglist'), # emtpy argument list
 }
 
-fns = {
- 'inc':  (20,1,'inc/1'), # def inc(x): return add2(x,1)
- 'add2': (20,2,'add/2'), # def add2(x,y): return x+y
- 'sub2': (20,2,'sub/2'), # def sub2(x,y): return x-y
- 'add3': (20,3,'add/3'), # def add3(x,y,z): return x+y+z
-}
-
-
 class Parser:
  def __init__(s,tokens):
-  s.state = 'unary' # current state
-  s.o = [] # operator stack [(bp,arity,description)]
-  s.d = [] # Ast
-  s.s = tokens # [::-1]
-  s.i = 0 # current token pointer
-  s.z = len(tokens)
- def __repr__(s): # return ' '.join(s.s[:s.i])+'¦'+' '.join(s.s[s.i:])
+  s.state = 'unary' # current state : Literal['unary','binary','error','done']
+  s.o = []          # operator stack : List[Tuple[bp:int,arity:int,description:str]]
+  s.d = []          # AST node stack : List(Ast) (should end with 0 or 1)
+  s.s = tokens      # raw string : str
+  s.i = 0           # index of current token : int
+  s.z = len(tokens) # last index + 1 : int
+ def __repr__(s):   # return ' '.join(s.s[:s.i])+'¦'+' '.join(s.s[s.i:])
   o = ' '.join(x[2] for x in s.o)
   d = ' '.join(map(repr,s.d))
-  t = f'«{s.state}»'
-  return f'{s.s[s.i] if s.i<s.z else " ":<5} • {o:<20} • {d:<20} {t}'
- def top(s): return s.o[-1] if s.o else None
+  t = f'{s.state}'
+  return f'[{o:>30}]  [{d:<30}]  {t:<8}  {s.s[s.i] if s.i<s.z else " ":<5}'
+ def peeko(s): return s.o[-1] if s.o else None
+ def popd(s): return s.d.pop()
+ def popo(s): return s.o.pop()
+ def pushd(s,x): s.d.append(x)
+ def pusho(s,x): s.o.append(x)
  def setstate(s,newstate,msg=''):
   if msg: debug(msg)
-  if s.state in ('done','error'): return
+  if s.state in ('done','error'): return # don't change either of these states
   s.state = newstate
- def end(s): return s.i >= s.z
- def next(s,inc=1) -> str|None:
-  if s.i<s.z: x=s.s[s.i]; s.i+=inc; return x
+ def end(s): return s.i>=s.z
+ def next(s,inc=1)->str|None: # doubles as peek() when inc=0
+  if s.end(): return None
+  x = s.s[s.i]
+  s.i += inc
+  return x
+
 
  def reduce_until(s,*matches):
-  while s.top() not in matches:
-   debug('(until)',s)
+  print('until')
+  p,arity,name = s.peeko()
+  while s.peeko() not in matches:
+   debug(s,f'until{matches}')
    if not s.o:
-    return s.setstate('error')
-   op,arity,name = s.o.pop()
-   args = [s.d.pop() for _ in range(arity)]
-   s.d.append(Ast(name,*(args[::-1])))
+    return s.setstate('error','unexpected emtpy stack')
+   p,arity,name = s.popo()
+   if name == 'fun':
+    p,arity,name = s.popo() # discard 'fun' and use actual function (pushed earlier)
+  args = [s.popd() for _ in range(arity)][::-1]
+  s.pushd(Ast(name,*args))
 
- def reduce(s,bp):
+
+ def reduce(s,src:dict,op:str):
   while s.o:
-   debug(s)
-   p,arity,name = s.top()
-   if p >= bp:
-    break
-   # FIXME: fun should pop argument name and argument list
-   # Currently, we put the argument name on the stack,
-   # but the argument list should have already been reduced to
-   # (semi arg1 arg2 ... argN) so it looks like a single argument
-   if len(s.d) < arity:
-    return s.setstate('error',"'parse ERROR: not enough arguments on data stack")
-   s.o.pop() # discard top (previous s.top() provided all we need)
-   s.d.append(Ast(name,*[s.d.pop() for _ in range(arity)][::-1]))
+   p,arity,name = s.peeko()
+   # if name in ('fun','list'): return
+   if p >= src[op][0]: return # done for now
+   if len(s.d) < arity: return s.setstate('error',"not enough arguments on data stack")
+   _ = s.popo() # discard
+   args = [s.popd() for _ in range(arity)][::-1]
+   s.pushd(Ast(name,*args))
+
 
  def unary(s):
   op = s.next()
-  while op and op.isspace(): op = s.next()
+  s.setstate('binary')
+  while op and op.isspace(): op = s.next() # consume whitespace tokens
   if op is None: return s.setstate('done')
-  if op.isnumeric():
-   nums = [Ast(op)]
+  if op.isnumeric() or op=='-':
+   if op=='-':
+    if not s.next(0).isnumeric():
+     s.setstate('unary')
+     s.pusho(uop['-'])
+     return
+    nums = [Ast(op+s.next())]
+   else:
+    nums = [Ast(op)]
    while (op := s.next()):
     if op.isspace(): continue
-    if op.isnumeric(): nums.append(Ast(op))
-    else: s.i-=1; break
-   s.d.append(Ast('array',*nums) if len(nums)>1 else Ast(*nums))
-   s.setstate('binary')
-  elif op.isalnum(): # name
-   s.d.append(Ast(op))
-   s.setstate('binary')
-  elif op in uop: s.o.append(uop[op])
+    elif op.isnumeric(): nums.append(Ast(op))
+    elif op=='-' and s.next(0).isnumeric(): nums.append(Ast('-'+s.next()))
+    else: s.i -= 1; break
+   print(nums)
+   s.pushd(Ast('array',*nums) if len(nums)>1 else Ast(*nums))
+  elif op.isalnum(): s.pushd(Ast(op)) # name
+  elif op in uop:
+   s.setstate('unary')
+   if op == ']' and s.peeko() == bop['[']: return
+   s.pusho(uop[op])
   else: s.setstate('error')
+
 
  def binary(s):
   op = s.next()
-  while op and op.isspace(): op = s.next()
+  while op and op.isspace(): op = s.next() # consume whitespace tokens
   if op is None: return s.setstate('done')
 
+  # FIXME: TESTING
+  if op in bop:
+   s.reduce(bop,op)
+   s.pusho(bop[op])
+   s.setstate('unary')
+  return
+
   if op == '[': # in binary state, '[' means function call
-   fn = s.d.pop().node # function name
-   s.reduce(bop[op][0])
-   s.o.append(fns[fn]) # push func to op stack
-   s.o.append(bop[op]) # show reduce_until where to stop
+   s.reduce(bop,op)
+   fn = s.popd().node # name, assume it's a function
+   s.pusho((10,None,fn)) # push name to op stack (don't know arity)
+   s.pusho(bop[op]) # show reduce_until where to stop
    s.setstate('unary')
 
   elif op == ']':
-   s.reduce(bop[op][0])
-   # s.reduce_until(bop['['])
-   # args = s.d.pop()
-   # tmp = []
-   # while args.node == 'semi':
-   #  first,args = args.children
-   #  tmp.append(first)
-   # if tmp:
-   #  s.d.append(Ast(s.o.pop()[2],*(tmp+[args])))
-   # else:
-   #  s.d.append(Ast(s.o.pop()[2],args))
-   # s.setstate('binary')
-   s.d.append(Ast(s.o.pop()[-1],s.d.pop()))
+   s.reduce_until(bop['[']) # consumes '[', pushes args to s.d
    s.setstate('binary')
 
-  elif op == ')':
-   s.reduce_until(uop['('])
-   args = s.d.pop()
-   tmp = []
-   while args.node == 'semi':
-    first,args = args.children
-    tmp.append(first)
-   if tmp:
-    s.d.append(Ast(s.o.pop()[2],*(tmp+[args])))
-   else:
-    s.d.append(Ast(s.o.pop()[2],args))
-   s.setstate('binary')
+  # elif op == ')':
+  #  s.reduce_until(uop['('])
+  #  args = s.d.pop()
+  #  tmp = []
+  #  while args.node == 'semi':
+  #   first,args = args.children
+  #   tmp.append(first)
+  #  if tmp:
+  #   s.pushd(Ast(s.o.pop()[2],*(tmp+[args])))
+  #  else:
+  #   s.pushd(Ast(s.o.pop()[2],args))
+  #  s.setstate('binary')
 
   elif op in bop:
-   s.reduce(bop[op][0])
-   s.o.append(bop[op])
+   s.reduce(bop,op)
+   s.pushd(bop[op])
    s.setstate('unary')
 
   else:
    s.setstate('error')
 
  def parse(s):
+  debug(f' {"operators":^30}   {"data":^30}    state     next token')
   while True:
    if s.state == 'unary': debug(s); s.unary()
    elif s.state == 'binary': debug(s); s.binary()
    else: break
-  if s.state == 'done': s.reduce(100)
+  if s.state == 'done': s.reduce(bop,'')
   else:
    toks = ' '.join(s.s[:s.i])
    debug("'parse\n"+toks+'\n'+(len(toks)-1)*" "+"^")
