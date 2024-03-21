43189553887cdcc899242f35ac074021d26e1ae1
Alex Shroyer <ashroyer@gmail.com>
2024-03-21 17:24:03 -0400

    reimplement with overloaded ()

--- src/misc/ee.py
+++ src/misc/ee.py
@@ -1,39 +1,31 @@
 # https://erikeidt.github.io/The-Double-E-Method
 # recursive descent parsing for statements, blocks, and expressions
 from Scanner import Scanner
-
 debug = print
 # def debug(*args,**kwargs): pass
 
-bop = { #{token : (bp, arity, name)}
+bop = { # token:(order,arity,name) high order binds later than low order
  '+': (5,2,'add'),
- '-': (5,2,'sub'), # subtract
- '*': (5,2,'mul'), # multiply
- '%': (5,2,'div'), # divide
- '^': (5,2,'pow'), # power
- '=': (5,2,'equ'), # equals
- ':': (5,2,'imm'), # immutable name binding
- '::':(5,2,'mut'), # mutable binding or update
- '[': (6,2,'arg'), # pops: name from s.o, arglist from s.d
+ '-': (5,2,'sub'),
+ '*': (5,2,'mul'),
+ '[': (6,2,'osq'), # function call
  ';': (6,2,'sep'), # argument or list item separator
- # ']': (8,0,'gra'), # arglist
- # ')': (8,0,'cpa'),
+ ']': (8,0,'csq'), # arglist
+ ')': (8,0,'cpa'), # close paren
  '':  (9,0,'fin'), # end of tokens
 }
 
 uop = {
- '-': (5,1,'neg'), # negate
- '*': (5,1,'fir'), # first
  '+': (5,1,'flp'), # flip
- '=': (5,1,'grp'), # group
- '%': (5,1,'sqr'), # square root
- # ';': (6,1,'skp'), # skip
- '(': (6,0,'opa'), # open paren for (grouping) (and;lists)
+ '-': (5,1,'neg'), # negate
+ '(': (6,1,'opa'), # open paren for (grouping-items) (or;for;lists)
  '[': (6,1,'prg'), # progn
  ')': (8,0,'cpa'), # close paren
- ']': (0,0,'nil'), # emtpy argument list "nilad"
+ ']': (8,0,'csq'), # f[] (nilad)
+ '':  (9,0,'fin'), # end of tokens
 }
 
+OW,DW = 20,30
 class Parser:
  def __init__(s,tokens):
   s.state = 'unary' # current state : Literal['unary','binary','error','done']
@@ -42,18 +34,24 @@ class Parser:
   s.s = tokens      # raw string : str
   s.i = 0           # index of current token : int
   s.z = len(tokens) # last index + 1 : int
+  s.m = ''
  def __repr__(s):   # return ' '.join(s.s[:s.i])+'¦'+' '.join(s.s[s.i:])
   o = ' '.join(x[2] for x in s.o)
   d = ' '.join(map(repr,s.d))
   t = f'{s.state}'
-  return f'[{o:<30}]  [{d:<50}]  {t:<8}  "{s.s[s.i] if s.i<s.z else ""}"'
+  return f'[{o:<{OW}}]  [{d:<{DW}}]  {t:<8}  "{s.s[s.i] if s.i<s.z else ""}"'
  def peekd(s): return s.d[-1] if s.d else None
  def peeko(s): return s.o[-1] if s.o else None
  def popd(s): return s.d.pop()
  def popo(s): return s.o.pop()
  def pushd(s,x): s.d.append(x)
  def pusho(s,x): s.o.append(x)
- def error(s,msg): debug('ERROR: '+msg);s.state = 'error'
+ def error(s,msg):
+  s.state = 'error'
+  s.m = msg
+  print("'PARSE:",s.m)
+  print(' '.join(s.s[:s.i]),' '.join(s.s[s.i:]))
+  print((len(toks)-1)*" "+"^")
  def setstate(s,newstate):
   if newstate not in ('binary','unary','done'): raise
   if s.state in ('done','error'): return # don't change either of these states
@@ -66,13 +64,6 @@ class Parser:
   return x
 
  def unary(s):
-  '''
-  In unary state, a closing token like ")" or "]" implies projection as in
-  "(3+)" and "f[x;]" (meanwhile "f[;x]" is another kind of projection) or a
-  progn like "[x;]" with an empty final expression.
-
-  Else, we have "(" or "[" to start a list or progn, or unary ops, or operands.
-  '''
   op = s.next()
   s.setstate('binary')
   while op and op.isspace(): op = s.next() # consume leading whitespace
@@ -95,96 +86,65 @@ class Parser:
      nums.append(Ast('-'+s.next()))
      space = 0
     else: s.i -= 1;break
-   s.pushd(Ast('array',*nums) if len(nums)>1 else Ast(*nums))
+   s.pushd(Ast('array',*nums) if len(nums)>1 else Ast(*nums)) # array or int
   elif op.isalnum(): s.pushd(Ast(op)) # name
-
-  # handle ] or ) in unary state which must be one of:
-  # - empty arglist: f[]
-  # - function projection: f[a;]
-  # - list/progn ending with nil: (a;) [a;]
-  # - parenthesized projection: (2+)
-
-  elif op == ']':
-   # s.reduce_square(op)
-   if s.peeko()[2] in ('prg','sep'): # [;]
-    s.pushd(Ast('nil'))
-    s.reduce_square(op)
-   elif s.popo()[2] == 'arg': # f[]
-    s.pushd(Ast(s.popo()[2],Ast(uop[op][2])))
-   else:
-    return s.error('unmatched "]"')
-
+  elif op == ']': s.pushd(Ast('nil'));s.reduce_square(uop,op)
   elif op == ')':
-   if s.peeko()[2] in ('opa','sep'):
-    s.pushd(Ast('nil'))
-    s.reduce_paren(op)
-   elif s.popo()[2] == 'list':
-    s.pushd(Ast(s.popo()[2],Ast(uop[op][2])))
-   else:
-    s.error('unmatched ")"')
-
-  elif op == ';': # leading ";" as in (;x) or ";x" or "2+;x" - reduce up to ";"
-   s.pushd(Ast('nil'))
-   s.pusho(bop[op]) # NOTE: binary op even though we're in unary state, for (;m)
-   s.setstate('unary')
-
-  elif op in uop:
-   s.pusho(uop[op])
-   s.setstate('unary')
-
+   if s.peeko()[2] == 'opa': s.pushd(Ast('list',Ast('nil')))
+   else:s.pushd(Ast('nil'))
+   s.reduce_paren(uop,op)
+  elif op == ';': s.pushd(Ast('nil'));s.pusho(bop[op]);s.setstate('unary')
+  elif op in uop: s.pusho(uop[op]);s.setstate('unary')
   else: s.error(f'unidentified unary op: {op}')
 
-
- def reduce_top(s,src:dict,op:str):
-  p,arity,name = s.popo()
-  if len(s.d) < arity:
-   return s.error("not enough arguments on data stack")
-  args = [s.popd() for _ in range(arity)][::-1]
-  if name == 'sep' and args[-1].node == 'sep': # merge
-   a,b = args
-   s.pushd(Ast(name,a,*b.children)) # (cons a (cons b c)) => (a b c)
-   print(s,"merge sep")
-  elif name == 'prg' and args[-1].node == 'sep': # (prg (sep ...)) => (prg ...)
-   s.pushd(Ast(name,*args[-1].children))
-   print(s,'merge prg')
-  else:
-   s.pushd(Ast(name,*args))
 
  def reduce(s,src:dict,op:str):
   while s.o and s.state != 'error':
    p,arity,name = s.peeko()
-   if p>=src[op][0]: break
-   # if name in ('arg','opa') or p>=src[op][0]: break
+   if p>=src[op][0] or name in ('osq','opa'): break # stop if you reach "[" or "("
    s.reduce_top(src,op)
 
- def reduce_paren(s,op):
-  while s.o:
-   s.reduce(bop,op)
+ def reduce_top(s,src:dict,op:str):
+  p,arity,name = s.popo()
+  if len(s.d) < arity: s.error("not enough arguments on data stack"); return
+  args = [s.popd() for _ in range(arity)][::-1]
+  if name == 'sep' and args[-1].node == 'sep': # merge
+   a,b = args
+   s.pushd(Ast(name,a,*b.children)) # (cons a (cons b c)) => (a b c)
+  elif name == 'prg' and args[-1].node == 'sep': # (prg (sep ...)) => (prg ...)
+   s.pushd(Ast(name,*args[-1].children))
+  else: s.pushd(Ast(name,*args))
+
+ def reduce_paren(s,src,op):
+  while True:
+   s.reduce(src,op)
+   if not s.o: return
    p,arity,nm = s.peeko()
-   if nm == 'opa':
+   if nm == 'opa': # reduce until no more stack or found "("
     s.popo()
-    if s.peekd().node == 'sep': # merge
-     s.pushd(Ast('list',*s.popd().children)) # (a;b;c;d;e)
+    if s.peekd().node == 'sep': # merge ";"
+     k = s.popd().children
+     # if any(x.node == 'nil' for x in k): return s.error("empty expression in list")
+     s.pushd(Ast('list',*k))
     break
    args = [s.popd() for _ in range(arity)][::-1]
    s.pushd(Ast(nm,*args)) # reduce top operator
 
- def reduce_square(s,op):
+ def reduce_square(s,src,op):
   while True:
-   s.reduce(bop,op)
+   s.reduce(src,op)
    if not s.o: return
    p,arity,nm = s.peeko()
-   if nm == 'arg':
+   if nm == 'osq':
     s.popo()
-    break # reduce until 'arg', then build funcall node
+    break # reduce until 'osq', then build funcall node
    if nm == 'prg':
     break
    args = [s.popd() for _ in range(arity)][::-1]
    s.pushd(Ast(nm,*args)) # reduce top operator
-  # if s.peekd() is None:
-  #  s.pushd(Ast(nm,s.popo()))
-  if s.peekd().node == 'sep': # merge
-   print("merging")
+  if s.peekd() is None:
+   s.pushd(Ast(nm,s.popo()))
+  elif s.peekd().node == 'sep': # merge
    s.pushd(Ast(s.popo()[2],*s.popd().children)) # [x;y;z]
   else:
    s.pushd(Ast(s.popo()[2],s.popd()))
@@ -193,40 +153,29 @@ class Parser:
   op = s.next()
   while op and op.isspace(): op = s.next() # consume whitespace tokens
   if op is None: return s.setstate('done')
-
   elif op == '[': # in binary state, '[' means 'call'
    fn = s.popd().node # extract function name
    s.pusho((5,-1,fn)) # push name to op stack (defer arity to runtime)
    s.pusho(bop[op]) # show ] where to stop
    s.setstate('unary')
-
-  elif op == ')':
-   s.reduce_paren(op)
-
-  elif op == ']':
-   s.reduce_square(op)
-   s.setstate('binary')
-
+  elif op == ')': s.reduce_paren(bop,op)
+  elif op == ']': s.reduce_square(bop,op)
   elif op in bop:
    s.reduce(bop,op)
    s.pusho(bop[op])
    s.setstate('unary')
-
-  else:
-   s.setstate('error')
+  else: s.error('unknown binary op')
 
  def parse(s):
-  debug(f' {"operators":^30}   {"data":^50}    state     next token')
-  while True:
-   if s.state == 'unary': debug(s); s.unary()
-   elif s.state == 'binary': debug(s); s.binary()
-   else: break
-  if s.state == 'done': s.reduce(bop,'')
-  else:
-   toks = ' '.join(s.s[:s.i])
-   debug("'parse\n"+toks+'\n'+(len(toks)-1)*" "+"^")
-  assert len(s.d)<2, "leave at most one AST node on the stack"
-  return s.d[0] if len(s.d) else []
+  debug(f' {"operators":^{OW}}   {"data":^{DW}}    state     next token')
+  while s.state in ('binary','unary'):
+   s.unary() if s.state=='unary' else s.binary()
+   debug(s)
+  if s.state == 'error': return None
+  s.reduce(bop,'')
+  debug(s)
+  if s.o and s.peeko()[2] in ('opa','osq','prg'): return s.error("unmatched bracket/paren")
+  return s.d[0] if len(s.o)==0 and len(s.d)==1 and s.state=='done' else None
 
 
 class Ast:
@@ -234,10 +183,12 @@ class Ast:
  def __init__(s,node,*children):
   s.node = node
   s.children = children
+ # def __eq__(s,other):
+ #  return s.node == other.node and s.children == other.children
  def __repr__(s):
-  if not s.children: return f'{s.node}'
-  # return f'({s.node} {" ".join(map(repr,s.children))})' # lisp style
-  return f'{s.node}({",".join(map(repr,s.children))})' # c-style
+  if not s.children and s.node != 'list': return f'{s.node}'
+  return f'({s.node} {" ".join(map(repr,s.children))})' # lisp style
+  # return f'{s.node}({",".join(map(repr,s.children))})' # c-style
   # return f'{" ".join([repr(x) for x in s.children[::-1]])} {s.node}' # forth style (reverse args)
 
 
---
+++ src/misc/ee2.py
@@ -0,0 +1,188 @@
+# https://erikeidt.github.io/The-Double-E-Method
+# recursive descent parsing for statements, blocks, and expressions
+from typing import List
+debug = print
+def _debug(*a,**b): pass
+
+bop = { # token:(order,arity,name) high order binds later than low order
+ '(': (1,1,'fun'), # function call
+ '*': (2,2,'mul'),
+ '%': (2,2,'div'),
+ '+': (2,2,'add'),
+ '-': (2,2,'sub'),
+ ';': (4,2,'seq'), # expression or argument separator
+ ')': (9,0,'cpa'), # close paren
+ '':  (9,0,'fin'), # no more tokens
+}
+
+uop = {
+ '-': (2,1,'neg'),
+ ')': (3,0,'arg'), # empty arglist
+ '(': (9,1,'opa'), # list
+}
+
+OW,DW = 20,20
+class Parser:
+ def __init__(s,tokens:List[str]):
+  s.state = 'unary' # current state : Literal['unary','binary','error','done']
+  s.o = []          # operator stack : List[Tuple[bp:int,arity:int,description:str]]
+  s.d = []          # AST node stack : List(Ast) (should end with 0 or 1)
+  s.s = tokens      # raw string : str
+  s.i = 0           # index of current token : int
+  s.z = len(tokens) # last index + 1 : int
+  s.m = ''
+ def __repr__(s):
+  o = ' '.join(x[2] for x in s.o)
+  d = ' '.join(map(repr,s.d))
+  t = f'{s.state}'
+  return f'[{o:<{OW}}]  [{d:<{DW}}]  {t:<8}  "{s.s[s.i] if s.i<s.z else ""}"'
+ def peeko(s): return s.o[-1] if s.o else None
+ def popd(s): return s.d.pop()
+ def popo(s): return s.o.pop()
+ def pushd(s,x): s.d.append(x)
+ def pusho(s,x): s.o.append(x)
+ def error(s,msg):
+  s.state = 'error'
+  s.m = msg
+  toks = ' '.join(s.s[:s.i])
+  print("'PARSE:",s.m)
+  print(toks,' '.join(s.s[s.i:]))
+  print((len(toks)-1)*" "+"^")
+ def setstate(s,newstate):
+  if newstate not in ('binary','unary','done'): raise
+  if s.state in ('done','error'): return # don't change either of these states
+  s.state = newstate
+ def end(s): return s.i>=s.z
+ def next(s,inc=1): # doubles as peek() when inc=0
+  if s.end(): return None
+  x = s.s[s.i]
+  s.i += inc
+  return x
+
+
+ def unary(s):
+  s.setstate('binary') # we'll usually (but not always) switch after unary state
+  if (op:=s.next()) is None:
+   s.setstate('done')
+  elif op.isnumeric() or op=='-':
+   if op=='-':
+    if not s.next(0).isnumeric():
+     s.pusho(uop[op]) # unary minus
+     s.setstate('unary')
+     return
+    s.pushd(Ast(op+s.next()))
+   else:
+    s.pushd(Ast(op))
+  elif op.isalnum():
+   s.pushd(Ast(op)) # name
+  elif op==';':
+   s.pushd(Ast('()')) # add "()" left argument to ","
+   s.pusho(bop[op]) # treat as binary
+   s.setstate('unary')
+  elif op==')':
+   if s.peeko()[2] in ('fun','seq'):
+    s.pushd(Ast('()'))
+    s.reduce_paren()
+   else:
+    s.pushd(Ast('()'))
+    s.popo()
+  elif op in uop:
+   s.pusho(uop[op])
+   s.setstate('unary')
+  else: s.error(f'unidentified unary op: {op}')
+
+
+ def reduce(s,prec:int,*matches:tuple):
+  while s.o and s.state != 'error':
+   p,arity,name = s.peeko()
+   if len(s.d)<arity: return
+   if matches and name in matches: return
+   if name=='fun': return
+   if p>=prec: return
+   s.popo()
+   args = [s.popd() for _ in range(arity)][::-1]
+   if name=='seq' and args[1].node=='seq':
+    a,b = args
+    bs = (Ast(x) for x in b.children)
+    s.pushd(Ast(name,a,*bs)) # (cons a (cons b c)) => (a b c)
+   else:
+    s.pushd(Ast(name,*args))
+
+
+ def reduce_paren(s):
+  s.reduce(9)
+  if not s.o: return s.error('unmatched ")"')
+  t = s.popo()
+  if not t or t[2] not in ('fun','opa'):
+   return s.error('unmatched ")"')
+  args = s.popd()
+  fn = s.popd() if t[2]=='fun' else t[2]
+  ch = args.children if args.node=='seq' else (args,) # tuple-ify for next step: *ch
+  if fn=='opa':
+   s.pushd(Ast('lst',*ch) if args.node=='seq' else Ast(*ch))
+  else:
+   s.pushd(Ast(fn,*ch))
+
+ def binary(s):
+  if (op:=s.next()) is None:
+   s.setstate('done')
+  elif op == ')': # (a;b) and function calls: f(x;y)
+   s.reduce_paren()
+  elif op in bop:
+   s.reduce(bop[op][0])
+   s.pusho(bop[op])
+   s.setstate('unary')
+  else:
+   s.error('unknown binary op')
+
+
+ def parse(s):
+  debug(f' {"operators":^{OW}}   {"data":^{DW}}    state     next token')
+  while s.state in ('binary','unary'):
+   debug(s)
+   s.unary() if s.state=='unary' else s.binary()
+  s.reduce(bop[''][0]) # reduce harder
+  if s.state == 'error':
+   return None
+  if s.o == [bop[';']]:
+   return Ast('shy',s.popd())#  if len(s.d)<2 else 'seq',s.popd())
+  if len(s.d)==1:
+   return s.popd()
+  debug(s,'after last reduce')
+  if s.o and s.popo()[2]=='seq':
+   return Ast('shy',*s.d) if len(s.d)==1 else Ast('seq',*s.d)
+  return s.d[0] if len(s.o)==0 and len(s.d)==1 and s.state=='done' else None
+
+
+class Ast:
+ '''node and optional children'''
+ def __init__(s,node,*children):
+  s.node = node
+  s.children = children
+ def __repr__(s):
+  if not s.children and s.node != 'list': return f'{s.node}'
+  return f'({s.node} {" ".join(map(repr,s.children))})' # lisp style
+
+def parse(s:str):
+ '''tokens are single characters'''
+ return Parser(list(s)).parse()
+
+def test(verbose=0):
+ global debug
+ debug = print if verbose else _debug
+ assert repr(parse("-a+b-c")) == "(neg (add a (sub b c)))"
+ assert repr(parse("-(a+b)-c")) == "(neg (sub (add a b) c))"
+ assert repr(parse("(-a)+b-c")) == "(add (neg a) (sub b c))"
+ assert repr(parse("a+b;0;c-d")) == "(seq (add a b) 0 (sub c d))"
+ assert repr(parse("f()")) ==  "(f ())"
+ assert repr(parse("f(x)")) ==  "(f x)"
+ assert repr(parse("f(x;)")) ==  "(f x ())"
+ assert repr(parse("f(;x)")) ==  "(f () x)"
+ assert repr(parse("f(x;y)")) ==  "(f x y)"
+ assert repr(parse("f(-a;b)")) ==  "(f (neg a) b)"
+ assert repr(parse("x+f()")) ==  "(add x (f ()))"
+ assert repr(parse("x;f()")) == "(seq x (f ()))"
+ assert repr(parse("-a;c")) == "(seq (neg a) c)"
+
+if __name__ == "__main__":
+ test()
