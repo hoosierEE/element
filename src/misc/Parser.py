419ec0450971ca5d044365b068ee25601e838e98
Alex Shroyer <ashroyer@gmail.com>
2024-04-04 17:05:42 -0400

    small tweaks

--- src/misc/Parser.py
+++ src/misc/Parser.py
@@ -1,11 +1,5 @@
 from collections import namedtuple
-class Ast:
- '''node and optional children'''
- def __init__(s,node,*children):
-  s.node = node
-  s.children = children
- def __repr__(s):
-  return f'({s.node} {" ".join(map(repr,s.children))})' if s.children else str(s.node)
+from Ast import Ast
 NIL = Ast('NIL')
 Op = namedtuple('Op','name arity')
 adverb = "'/\\"
@@ -21,11 +15,9 @@ lookup = {
  Op('[',2):'call',#f[x]
  Op(';',2):'seq',#x;y
 }
-tokenize = list#TODO: implement real tokenizer
-isname = str.isalnum#TODO: cooperate with tokenizer on this
-def parse(text,verbose=0):
- if not text: return
- t,z,b,s,d = tokenize(text),len(text),[],[],[]
+def parse(t,verbose=0):
+ if not t: return
+ t,z,b,s,d = list(t),len(t),[],[],[]
  def debug(*args):
   if not verbose: return
   ss = ' '.join(f'{lookup.get(x,x.name)}.{x.arity}' for x in s)
@@ -59,7 +51,7 @@ def parse(text,verbose=0):
      while i<z and t[i] in adverb: c,i = c+t[i],i+1
      if s and s[-1].name=='bind': s.pop(); s.append(Op(c,3))
      else: s.append(Op(c,2))
-    elif isname(c) or i<z and t[i] in adverb: d.append(Ast(c)); break
+    elif c.isalnum() or i<z and t[i] in adverb: d.append(Ast(c)); break
     else: s.append(Op(c,1))
 
    while True:#binary
@@ -80,16 +72,15 @@ def parse(text,verbose=0):
  debug('done')
  reduce('')
  r = d.pop()
- if len(d) or len(s): return print("ERROR: stacks should be empty")
+ if len(d) or len(s): raise SyntaxError("ERROR: stacks should end up empty")
  return r
 
-def test():
- #TODO:juxtaposition:  (a)b and b(a)
- #TODO:symbols: ``a`b
- tests = (
-  " a ab abc abcd abcde (a) (a;b) (;b) (a;) (;) (a;;b) a;b ;b a; ; a;;b a+b a++b"
+def test():#TODO:juxtaposition:  (a)b and b(a), symbols
+ x = (
+  " a -a --a aa aaa a-b a-b+c (a) (a;b) (;b) (a;) (;) (a;;b) a;b ;b a; ; a;;b a+b a++b"
   " [] [x] f[] f[x] f[x;y] f[;y] f[x;] f[;] f[x;;y] a+f[x]"
   " x/y +/y x+/y xf/y (f)/y (+)/y (-a)*b"
   " f//y xf//y (+//)'y x(+/)//'y x(af/)'y").split()
- ml = max(map(len,tests))
- for t in tests: print(f'{t:<{ml}} ⇒ {parse(t)}')
+ m = max(map(len,x))
+ for t in x: print(f'{t:<{m}} ⇒ {parse(t)}')
+if __name__=='__main__': test()
