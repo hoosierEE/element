3c7c0845dc557762fa30b3f448b903549f5c9da6
Alex Shroyer <ashroyer@gmail.com>
2024-04-11 14:42:21 -0400

    juxtaposition as application is tricky

--- src/misc/Ast.py
+++ src/misc/Ast.py
@@ -1,12 +1,8 @@
-# from dataclasses import dataclass
-# @dataclass
 class Ast:
  '''(node [children])'''
  def __init__(s,node,*children):
   s.node = node
   s.children = children
- # def __eq__(s,o):
- #  return s.node==o.node and s.children==o.children
  def __repr__(s):
   n = dict(zip("{[(;/\\'",'lam prg lst seq fld scn ech'.split())).get(s.node,s.node)
   return f'({n} {" ".join(map(repr,s.children))})' if s.children else str(n)
--- src/misc/Parser.py
+++ src/misc/Parser.py
@@ -2,20 +2,11 @@ from collections import namedtuple
 from Ast import Ast
 NIL = Ast('NIL')
 Op = namedtuple('Op','name arity')
-lookup = {
- ('{',1):'lam',#{x+y}/z
- ('{',2):'lam',#n{x+y}/z
- ('(',1):'lst',#or to override precedence: (-a)+b
- ('(',2):'lst',#n(x) as in n(|+\)\1 1
- ('[',1):'prg',#[x+1;y] ⇒ y
- ('[',2):'fun',#f[x]
- (';',2):'seq',#x;y
-}
 adverb = "'/\\"
 oparen = '({['
 cparen = ')}]'
 semico = ';'
-def parse(t,verbose=0):
+def parse(t:str,verbose:int=0)->Ast:
  if not t: return
  t,z,b,s,d = list(t),len(t),[],[],[]
  def debug(*args):
@@ -28,11 +19,11 @@ def parse(t,verbose=0):
   if op in oparen: return b.append(cparen[oparen.index(op)])
   if op in cparen and (not b or op!=b.pop()): raise SyntaxError('unbalanced paren')
 
- def reduce(until):
+ def reduce(until:str):
   while s and str(s[-1].name) not in until:
    r1(s.pop())
 
- def r1(x):
+ def r1(x:Op):
   k,x = [d.pop() for _ in range(x.arity)][::-1],x.name
   if x==';':
    if k[1].node==x: k = [k[0],*k[1].children]
@@ -40,11 +31,14 @@ def parse(t,verbose=0):
   d.append(Ast(x,*k))
   debug('r1')
 
- def rp(x):#called after reduce(oparen)
-  x,y = lookup[x],d.pop()
-  y = (y.children if y.node==';' else (y,))
-  k = Ast(x,*((d.pop(),) if x=='fun' else ()),*y)
-  d.append(k.children[0] if x=='lst'and len(k.children)==1 else k)
+ def rp(x:Op,n:str):#called after reduce(oparen)
+  #FIXME: {x{y}} ⇒ (lam (lam x y)) should be (lam (x (lam y)))
+  y = (y.children if (y:=d.pop()).node==';' else (y,))
+  if   x==Op('[',2): k = Ast('fun',d.pop(),*y)
+  elif x==Op('{',3): k = Ast('lam',d.pop(),*y)
+  else: k = Ast(x.name,*y)
+  if x.name=='(' and len(k.children)==1 and k.children[0]!=NIL: k = k.children[0]
+  d.append(k)
   debug('rp')
 
  def loop(i=0):
@@ -52,41 +46,51 @@ def parse(t,verbose=0):
    while True:#unary
     if i>=z: return
     c,i,n = t[i],i+1,t[i+1]if i+1<z else''; balance(c); debug(c,'→',n or 'END')
-    if   c == semico and not n: d.append(NIL)
-    if   c == semico: d.append(NIL); reduce(oparen); s.append(Op(c,2))
-    elif c in cparen: d.append(NIL); debug('test'); reduce(oparen); rp(s.pop()); break
+    if   c == semico:
+     if not n: d.append(NIL)
+     d.append(NIL); reduce(oparen); s.append(Op(c,2))
+    elif c in cparen:
+     d.append(NIL); debug('cparen →'); reduce(oparen); rp(s.pop(),n)
+     if s and s[-1]==Op('{',1) and n!='}': s.pop(); s.append(Op('{',3)); continue
+     break
     elif c.isalnum(): d.append(Ast(c)); break
     elif c in oparen: s.append(Op(c,1));
-    elif (not n) or (n and n in cparen): d.append(Ast(c))
+    elif (not n) or (n in cparen): d.append(Ast(c))
     else: s.append(Op(c,1))
 
    while True:#binary
     if i>=z: return
     c,i,n = t[i],i+1,t[i+1]if i+1<z else''; balance(c); debug(c,'↔',n or 'END')
-    if   c == semico and not n: d.append(NIL)
-    if   c == semico: reduce(oparen); s.append(Op(c,2))
-    elif c in cparen: reduce(oparen); rp(s.pop()); continue
+    if   c == semico:
+     if not n: d.append(NIL);
+     reduce(oparen); s.append(Op(c,2))
+    elif c in cparen:
+     reduce(oparen); debug('cparen ↔'); rp(s.pop(),n);
+     if not(s and s[-1].name=='{' and n!='}'): continue
+     s.pop(); s.append(Op('{',3))
     elif c in adverb: s.append(Op(Ast(c,d.pop()),1))
-    elif c.isalnum(): s.append(Op(repr(d.pop()),1)); d.append(Ast(c)); continue
+    elif c.isalnum(): s.append(Op(repr(d.pop()),1)); d.append(Ast(c)); continue#apply
+    elif c in oparen: s.append(Op(repr(d.pop()),1)); s.append(Op(c,1))
     else: s.append(Op(c,2))
     break
 
  loop()
  debug('done')
- reduce([])
+ reduce('')
  while len(d)>1:
   x=d.pop();d.append(Ast(d.pop(),x))
  if len(d)!=1 or len(s) or len(b): return SyntaxError("ERROR: stacks should end up empty")
  return d.pop()
 
-def test():#TODO:juxtaposition:  (a)b and b(a), symbols
+def test():#(a)b and b(a), symbols
  x = (
-  " ab a(b) (a)b abc"
-  " a -a --a a-b a-b+c (a) (a;b) (;b) (a;) (;) (a;;b) a;b ;b a; ; a;;b a+b a++b"
+  " ab a(b) (a)b" # all same
+  " abc a -a --a a-b a-b+c () (a) (a;b) (;b) (a;) (;) (a;;b) a;b ;b a; ; a;;b a+b a++b"
   " [] [x] f[] f[x] f[x;y] f[;y] f[x;] f[;] f[x;;y] a+f[x]"
+  " +[] +[x] +[x;y] +[x;y;z] +[;] +[;;]"
+  " {} {x} {[]} {[a]} {[]a} {[a]b} {[a];b} {a;b} {ab} {x/y} {{}} {f[x]} {x}y {{x}[x]}"
   " x/y +/y x+/y xf/y (f)/y (+)/y (-a)*b"
   " f//y xf//y (+//)'y x(+/)//'y x(af/)'y"
  ).split()
  m = max(map(len,x))
- for t in x:
-  print(f'{t:<{m}} ⇒ ',end='');print(parse(t))
+ for t in x: print(f'{t:<{m}}',end=' ⇒ ');print(parse(t))
