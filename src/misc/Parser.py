0ff48a6a2725538c61ea899e6274f3392597d6c5
Alex Shroyer <ashroyer@gmail.com>
2024-04-12 14:25:19 -0400

    factor unit test out of Parser.py

--- src/misc/Parser.py
+++ src/misc/Parser.py
@@ -1,4 +1,5 @@
 from Ast import Ast
+from Parser_test import unit
 import collections as C
 NIL = Ast('NIL')
 Op = C.namedtuple('Op','name arity')
@@ -46,10 +47,11 @@ def parse(t:str,verbose:int=0)->Ast:
     if i>=z: return
     c,i,n = t[i],i+1,t[i+1]if i+1<z else''; balance(c); debug(c,'→',n or 'END')
     if   c == semico: [d.append(NIL)for _ in range(-~(not n))]; reduce(oparen); s.append(Op(c,2))
-    elif c in cparen:#goto binary
-     d.append(NIL); debug('cparen →'); reduce(oparen); x=s.pop(); rp(x);
-     break
-    elif c.isalnum(): d.append(Ast(c)); break
+    elif c in cparen:
+     d.append(NIL)# comment this, uncomment line 61 to fix adverbs
+     debug('cparen →'); reduce(oparen); x=s.pop(); rp(x); break
+    elif c in adverb: s.append(Op(Ast(c,Ast(s.pop().name)),1))
+    elif c.isalnum() or c in verb: d.append(Ast(c)); break
     elif c in oparen: s.append(Op(c,1))
     elif (not n) or (n in cparen): d.append(Ast(c))
     else: s.append(Op(c,1))
@@ -57,18 +59,18 @@ def parse(t:str,verbose:int=0)->Ast:
    while True:#binary
     if i>=z: return
     c,i,n = t[i],i+1,t[i+1]if i+1<z else''; balance(c); debug(c,'↔',n or 'END')
+    # if   n in cparen: d.append(NIL)# adverb workaround
     if   c == semico: 0 if n else d.append(NIL); reduce(oparen); s.append(Op(c,2))
-    elif c in cparen:#stay binary
-     debug('cparen ↔'); reduce(oparen); x=s.pop(); rp(x)
-     continue
+    elif c in cparen: debug('cparen ↔'); reduce(oparen); x=s.pop(); rp(x); continue
+    # elif n and n in cparen:
+    #  d.append(NIL)
+    elif c in verb+'[': s.append(Op(c,2))
     elif c in adverb:
      k = Ast(c,d.pop())#bind adverb func
-     if s and s[-1].arity==1 and str(s[-1].name) not in verb:#move left adverb arg to d
-      d.append(Ast(s.pop().name))
-      s.append(Op(k,2))
-     else:
-      s.append(Op(Ast(c,d.pop()),1))
-    elif c in verb: s.append(Op(c,2))
+     while n in adverb: k,i,n = Ast(n,k),i+1,t[i+1]if i+1<z else''
+     if s and s[-1].arity==1 and str(s[-1].name) not in (verb+oparen):
+      d.append(Ast(s.pop().name)); s.append(Op(k,2))
+     else: s.append(Op(k,1))
     else:
      if s and d and s[-1].name=='{' and d[-1].node=='[': s.append(Op(';',2))
      else: s.append(Op(d.pop(),1))
@@ -83,36 +85,3 @@ def parse(t:str,verbose:int=0)->Ast:
   x=d.pop();d.append(Ast(d.pop(),x))
  if len(d)!=1 or len(s) or len(b): return SyntaxError("ERROR: stacks should end up empty")
  return d.pop()
-
-def unit():
- x = """
- input  ‥ expected output
- {x{y}} ‥ (lam (x (lam y)))
- {x(y)} ‥ (lam (x y))
- {(x)y} ‥ (lam (x y))
- {()}   ‥ (lam (lst NIL))
- {()()} ‥ (lam ((lst NIL) (lst NIL)))
-        ‥ None
- {[]a}  ‥ (lam (prg NIL) a)
- {[]()} ‥ (lam (prg NIL) (lst NIL))
- abc    ‥ (a (b c))
- """[1:-1].splitlines()[1:]
- m = max(map(len,x))
- for i,o in (map(str.strip,a.split('‥')) for a in x):
-  if str(parse(i.strip()))!=o.strip():
-   s = f'{i} ⇒ {parse(i)}'
-   print(f'{s:<{m}}  expected:{o}')
-
-
-def test():#(a)b and b(a), symbols
- x = (
-  " ab a(b) (a)b" # all same
-  " abc a -a --a a-b a-b+c () (a) (a;b) (;b) (a;) (;) (a;;b) a;b ;b a; ; a;;b a+b a++b"
-  " [] [x] f[] f[x] f[x;y] f[;y] f[x;] f[;] f[x;;y] a+f[x]"
-  " +[] +[x] +[x;y] +[x;y;z] +[;] +[;;]"
-  " {} {x} {[]} {[a]} {[]a} {[a]b} {[a];b} {a;b} {ab} {x/y} {{}} {f[x]} {x}y {{x}[x]}"
-  " x/y +/y x+/y xf/y (f)/y (+)/y (-a)*b"
-  " f//y xf//y (+//)'y x(+/)//'y x(af/)'y"
- ).split()
- m = max(map(len,x))
- for t in x: print(f'{t:<{m}}',end=' ⇒ ');print(parse(t))
