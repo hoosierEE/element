99ef87d128c8cf7774e6d2367a02b41f389c0773
Alex Shroyer <ashroyer@gmail.com>
2024-04-23 11:10:10 -0400

    tidy up

--- src/misc/Parser.py
+++ src/misc/Parser.py
@@ -4,53 +4,53 @@ class Ast:#(node [children])
  def __repr__(s):
   n = dict(zip("{[(;/\\'",'lam prg lst seq fld scn ech'.split())).get(s.node,s.node)
   return f'({n} {" ".join(map(repr,s.children))})' if s.children else str(n)
+
 NIL,Op = Ast('NIL'),C.namedtuple('Op','name arity')
 verb,adverb,cparen,oparen,semico = '~!@#$%^&*-_=+|:,.<>?',"'/\\",')}]','({[',';'
 
 def parse(t:str,verbose:int=0)->Ast:
  if not t: return
  t,z,b,s,d = list(t),len(t),[],[],[]
+
  def debug(*args):
   if not verbose: return
   ss = ' '.join(f'{x.name}{"⁰¹²"[x.arity]}' for x in s)
   sd = ' '.join(map(str,d))
   print(f'[{ss:<19}] [{sd:<15}]',*args)
+
  def pad(n): n in cparen and d.append(NIL)
+
  def balance(op):
-  if op in oparen: return b.append(cparen[oparen.index(op)])
-  if op in cparen and (not b or op!=b.pop()): raise SyntaxError('unbalanced paren')
+  if op in oparen: b.append(cparen[oparen.index(op)]); return
+  if op in cparen and (not b or op!=b.pop()): return 1
 
  def reduce(until:str):
   while s and str(s[-1].name) not in until: rt(*s.pop())
 
- def rq(k:Ast):
+ def rq(k:Ast):#handle juxtaposition-based projections and copmositions
   while s and str(s[-1].name) not in oparen+semico:
-   x,a = s.pop()
-   if a==2: k = Ast('cmp',Ast('prj',Ast(x),d.pop()),k)
-   else:    k = Ast('cmp',Ast(x),k)
-  d.append(k)
-  debug('rq')
+   x,a = s.pop(); k = Ast('cmp',Ast('prj',Ast(x),d.pop()) if a==2 else Ast(x),k)
+  d.append(k); debug('rq')
 
  def rt(x,arity):#(r)educe (t)op of stack based on x's arity
   k = [d.pop() for _ in range(min(len(d),arity))][::-1]
   if x==';':
    if   len(k)>1 and k[1].node==x: k = [k[0],*k[1].children]
    elif len(k)>0 and k[0].node==x: k = [*k[0].children,*k[1:]]
-  d.append(Ast(x,*k))
-  debug('rt',x,k)
+  d.append(Ast(x,*k)); debug('rt',x,k)
 
  def rp(x:Op):#(r)educe (p)aren, e.g: reduce(oparen); rp(s.pop())
   k = Ast(x.name,*(y.children if (y:=d.pop()).node==';' else (y,)))
   if x.name=='(' and len(k.children)==1 and k.children[0]!=NIL: k = k.children[0]
   if x.name=='[' and x.arity==2: k = Ast(d.pop(),k)
-  d.append(k)
-  debug('rp',x,k)
+  d.append(k); debug('rp',x,k)
 
  def loop(i=0):
   while True:
    while True:#unary
     if i>=z: return
-    c,i,n = t[i],i+1,t[i+1]if i+1<z else''; balance(c); debug(c,'→',n or 'END')
+    c,i,n = t[i],i+1,t[i+1]if i+1<z else''; debug(c,'→',n or 'END')
+    if balance(c): return i
     if   c in semico: d.append(NIL); pad(n); reduce(oparen); s.append(Op(c,2))
     elif c in oparen: pad(n); s.append(Op(c,1))
     elif c in cparen:
@@ -66,7 +66,8 @@ def parse(t:str,verbose:int=0)->Ast:
 
    while True:#binary
     if i>=z: return
-    c,i,n = t[i],i+1,t[i+1]if i+1<z else''; balance(c); debug(c,'↔',n or 'END')
+    c,i,n = t[i],i+1,t[i+1]if i+1<z else''; debug(c,'↔',n or 'END')
+    if balance(c): return i
     if   c in semico: reduce(oparen); pad(n); s.append(Op(c,2))
     elif c in oparen: c in "({" and s.append(Op(d.pop(),2)); pad(n); s.append(Op(c,2))
     elif c in cparen:
@@ -92,7 +93,8 @@ def parse(t:str,verbose:int=0)->Ast:
      pad(n); s.append(Op(c,1))
     break
 
- loop(); debug('done'); reduce('')
- while len(d)>1: x=d.pop();d.append(Ast(d.pop(),x))
+ if i:=loop(): return print('unbalanced paren: '+"".join(t)+'\n'+f'{"^":>{18+i}}')
+ debug('done'); reduce('')
+ while len(d)>1: x=d.pop(); d.append(Ast(d.pop(),x))
  if len(d)!=1 or len(s) or len(b): return SyntaxError("ERROR: stacks should end up empty")
  return d.pop()
