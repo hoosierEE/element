682a28ab7ec1bc9021802e3e3ced20e2a84e5d27
Alex Shroyer <ashroyer@gmail.com>
2024-04-25 12:40:27 -0400

    start walking the AST

--- src/misc/Eval.py
+++ src/misc/Eval.py
@@ -1,6 +1,16 @@
 #input: AST
 #output: evaluate expressions to produce results, persist variables, side effects
 from Ast import Ast
-from Parser import parse
-def eval(t:Ast):
- ...
+from Parser import Parse
+from Scanner import Scan
+def Eval(ast):#recur depth-first, right-to-left
+ if ast.node=='arr':
+  print(ast.children)
+ else:
+  for c in ast.children[::-1]:
+   Eval(c)
+  if type(ast.node)==Ast:
+   Eval(ast.node)
+  else:
+   print(ast.node)
+ print(len(ast.children))
