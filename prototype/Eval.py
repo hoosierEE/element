c4624c13b04191b9e79aee6af5a06921a48800c5
Alex Shroyer <ashroyer@gmail.com>
2024-04-30 14:54:08 -0400

    less awful match/case

--- prototype/Eval.py
+++ prototype/Eval.py
@@ -2,89 +2,78 @@ from Ast import Ast#for type comparison
 from Parser import Parse; from Scanner import Scan#imported for repl convenience
 class Sym(str):pass
 class Name(str):pass
-# class Num(float,int):pass
+Ty = dict(zip((Sym,Name,float,int,str,list,dict),
+              's   n    f     i   c   l    d'.split()))
+
+def ty(x:str):#infer type
+ floaty = lambda x:x.count('.')==1 and x.replace('.','') or ''
+ if x[0] in '`"': return (str,Sym)[x[0]=='`']#str
+ elif x.isnumeric(): return int
+ elif floaty(x).isnumeric(): return float
+ elif x[0]=='-' and len(x)>1:
+  if x[1:].isnumeric(): return int
+  if floaty(x).replace('-','').isnumeric(): return float
+  else: return Name
+ else: return Name
+
 class Val:
  def __init__(s,v):
   if type(v)==Ast:
    if v.node!='vec': raise SyntaxError('not a vector')
-   s.vec = 1
    c = [c.node for c in v.children]
    if c0:=c[0][0] in '`"':
-    s.t = (str,Sym)[c0=='`']
+    s.t = Ty[(str,Sym)[c0=='`']].upper()
     s.v = c
    else:
-    s.t = (int,float)[any('.' in x for x in c)]
-    s.v = list(map(s.t,c))
+    t = (int,float)[any('.' in x for x in c)]
+    s.v = list(map(t,c))
+    s.t = Ty[t].upper()
   else:
-   s.vec = 0
-   s.t = s.ty(v)
-   s.v = s.t(v)
+   s.t = Ty[ty(v)]
+   s.v = ty(v)(v)
  def __repr__(s):
-  t = repr(s.t)[8:-2]
-  i = f'[{t}]' if s.vec else t
-  return f'{s.v}:{i}'
- def ty(s,x:str):#infer type
-  if x[0] in '`"': return (str,Sym)[x[0]=='`']#str
-  elif x.isnumeric(): return int
-  elif x.count('.')==1 and x.replace('.','').isnumeric(): return float
-  elif x[0]=='-' and len(x)>1:
-   if x[1:].isnumeric(): return int
-   if x.count('.')==1 and x.replace('.','').replace('-','').isnumeric(): return float
-   else: return Name
-  else: return Name
+  return f'{s.v}:{s.t}'
 
 class VVal(Val):
- def __init__(s,v,t,vec):
+ def __init__(s,v,t):
   s.v = v
   s.t = t
-  s.vec = vec
 
 verb,adverb = '~!@#$%^&*-_=+|:,.<>?',"'/\\"
 
-Num = int|float
-ops = {
- ('+',0b00,Num,Num): lambda x,y:x+y,
- ('+',0b01,Num,Num): lambda x,y:[x+yi for yi in y],
- ('+',0b10,Num,Num): lambda x,y:[xi+y for xi in x],
- ('+',0b11,Num,Num): lambda x,y:[xi+yi for xi,yi in zip(x,y)],
- ('-',0b00,Num): lambda y:-y,
- ('-',0b01,Num): lambda y:[-yi for yi in y],
- ('-',0b00,Num,Num): lambda x,y:x-y,
- ('-',0b01,Num,Num): lambda x,y:[x-yi for yi in y],
- ('-',0b10,Num,Num): lambda x,y:[xi-y for xi in x],
- ('-',0b11,Num,Num): lambda x,y:[xi-yi for xi,yi in zip(x,y)],
- ('#',0b00,Num): lambda y:1,
- ('#',0b01,Num): lambda y:len(y),
- ('#',0b00,int,Num): lambda x,y:[y]*x,
- ('#',0b01,int,Num): lambda x,y:y[x:] if x<0 else y[:x],
-}
-
-def Ops(op,vec,t1,t2):
- match (op,vec,t1,t2):
-  case ['+',0|1|2|3,'int'|'float','int'|'float']: return ops[op,vec,Num,Num]
-  case ['-',0|1|2|3,'int'|'float','int'|'float']: return ops[op,vec,Num,Num]
-  case ['-',0|1,'int'|'float',None]: return ops[op,vec,Num]
-  case ['#',0|1,'int'|'float',None]: return ops[op,vec,Num]
-  case ['#',0|1,'int','int'|'float']: return ops[op,vec,int,Num]
+def Ops(op,t1,t2):
+ match (op,t1,t2):
+  case ['+','f'|'i','f'|'i']:return lambda x,y:x+y
+  case ['+','f'|'i','F'|'I']:return lambda x,y:[x+yi for yi in y]
+  case ['+','F'|'I','f'|'i']:return lambda x,y:[xi+y for xi in x]
+  case ['+','F'|'I','F'|'I']:return lambda x,y:[xi+yi for xi,yi in zip(x,y)]
+  case ['-','f'|'i']:        return lambda y:-y
+  case ['-','F'|'I']:        return lambda y:[-a for a in y]
+  case ['-','f'|'i','f'|'i']:return lambda x,y:x-y
+  case ['-','f'|'i','F'|'I']:return lambda x,y:[x-a for a in y]
+  case ['-','F'|'I','f'|'i']:return lambda x,y:[y-a for a in x]
+  case ['-','F'|'I','F'|'I']:return lambda x,y:[a-b for a,b in zip(x,y)]
+  case ['#','i',_]:          return lambda x,y:y[x:] if x<0 else y[:x]
+  case ['#','i',None]:       return lambda y:1
+  case ['#',_,None]:         return lambda y:len(y)
+  case ['!','i',None]:       return lambda y:list(range(y))
   case _: raise Exception('nyi')
 
-def dispatch(v,e,x):
+def dispatch(v,e,*x):
  if len(x)==3:
   if v!='app': raise Exception('dispatch(3) only defined for apply')
   raise Exception('nyi')
  elif len(x)==2:
-  R,L = x #fix order (reverse reverse pop)
-  tl,tr = (repr(x)[8:-2] for x in (L.t,R.t))
-  A = L.vec*2+R.vec
+  R,L = x #undo reversal due to push/pop
+  A = L.t.isupper()*2+R.t.isupper()
   if A==3 and len(L.v)!=len(R.v): raise Exception('shape mismatch')
-  z = Ops(v,A,tl,tr)(L.v,R.v)
-  tz = type(z[0]) if type(z)==list else type(z)
-  return VVal(z,tz,type(z)==list)
+  z = Ops(v,L.t,R.t)(L.v,R.v)
+  tz = Ty[type(z[0])].upper() if type(z)==list else Ty[type(z)]
+  return VVal(z,tz)
  elif len(x)==1:
-  x = x[0]
-  t = repr(x.t)[8:-2]
-  r = Ops(v,x.vec,t,None)(x.v)
-  return VVal(r,x.t,x.vec)
+  z = Ops(v,x[0].t,None)(x[0].v)
+  tz = Ty[type(z[0])].upper() if type(z)==list else Ty[type(z)]
+  return VVal(z,tz)
  else:
   raise Exception('no dispatchable 0-argument ops exist')
 
@@ -101,8 +90,12 @@ def Eval(ast:Ast)->list:#depth-first, right-to-left
   for c in ks: _Eval(s,e,c)#evaluate children
   if type(n)==Ast: _Eval(s,e,n)#evaluate node if it's also an AST (i.e: adverb)
   else:
-   if n in verb:
-    s.append(dispatch(n,e,[s.pop() for _ in range(len(ks))]))
+   if ks and n in verb:
+    s.append(dispatch(n,e,*(s.pop() for _ in range(len(ks)))))
+   elif n=='app':
+    ... #TODO
+   elif n in adverb:
+    ... #TODO
    else:
     s.append(Val(n))
 
