61d5dc87a1c2ca1801d58f8c1d20853b92a63437
Alex Shroyer <ashroyer@gmail.com>
2024-05-14 15:24:06 -0400

    let Ast behave like a tuple of (node, *children)

--- prototype/Ast.py
+++ prototype/Ast.py
@@ -1,5 +1,6 @@
 class Ast:#(node children*)
- def __init__(s,n,*c): s.node,s.children = n,c
+ def __init__(s,n,*c): s.node,s.children = s.t = n,c
+ def __getitem__(s,i): return s.t[i]
  def __repr__(s):
   n = dict(zip("{[(;/\\'",'lam prg lst seq fld scn ech'.split())).get(s.node,s.node)
   return f'({n} {" ".join(map(repr,s.children))})' if s.children else str(n)
