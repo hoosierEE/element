f81a2e244dc16dde7edf0520abc0e00732d5a727
Alex Shroyer <ashroyer@gmail.com>
2024-07-25 17:47:33 -0400

    clean up

--- prototype/Eval.py
+++ prototype/Eval.py
@@ -1,26 +1,14 @@
-'''Eval.py - interpret a parsed expression'''
-type Sym = str
-type Name = str
-type Expr = float|list|str|Sym|Name
+type Expr = float|int|list|str
 
-def ty1(x:str) -> type:
- if x[0] in '`"': return (str,Sym)[x[0]=='`']
- if x.replace('.','').replace('-','').isnumeric(): return float
- return Name
+def ev(x:Expr)->Expr:
+ return Eval({},x)
 
-def infer_lit(x:Expr)->Expr:
- '''infer types for literals'''
- match x:
-  case ('vec',*a): t = [ty1(i) for i in a]
-  case _: raise TypeError(f'unmatched pattern: {x}')
-
-def ev(x:Expr)->Expr: return Eval({},infer_lit(x))
 def Eval(e:dict,x:Expr)->Expr:
  match x:
-  case float()|('lam',_,_):return x
-  case str():              return e[x]
-  case ('-',a,b):          return Eval(e,a)-Eval(e,b) # TODO: all the other operators
-  case ('let',a,b,c):      return Eval({**e,a:Eval(e,b)},c)
-  case ('if',a,b,c):       return Eval(e,b) if Eval(e,a) else Eval(e,c)
-  case (('lam',a,b),*c):   return Eval({**e,**dict(zip(a,(Eval(e,i) for i in c)))},b)
-  case list():             return Eval(e,[Eval(e,i) for i in x])
+  case float()|int()|('lam',_,_): return x
+  case str():                     return e[x]
+  case ('-',a,b):                 return Eval(e,a)-Eval(e,b) # TODO: all the other operators
+  case ('let',a,b,c):             return Eval({**e,a:Eval(e,b)},c)
+  case ('if',a,b,c):              return Eval(e,b) if Eval(e,a) else Eval(e,c)
+  case (('lam',a,b),*c):          return Eval({**e,**dict(zip(a,(Eval(e,i) for i in c)))},b)
+  case list():                    return Eval(e,[Eval(e,i) for i in x])
